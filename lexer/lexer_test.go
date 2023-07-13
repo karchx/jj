19a4156baa36ded9b7b4aec25454326177927a7b
Стив <kenethra.74@gmail.com>
2023-07-12 22:44:07 -0600

    feat: tokens EQ and NOT_EQ

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -102,7 +102,7 @@ func TestNextToken2(t *testing.T) {
 		{token.LPAREN, "("},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
-		{token.IDENT, "true"}, // change for type bool
+		{token.TRUE, "true"}, // change for type bool
 		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
 		{token.SEMICOLON, ";"},
@@ -135,6 +135,15 @@ func TestNextToken3(t *testing.T) {
 
     !-/*5;
     5 < 10 > 5;
+
+    if (5 < 10) {
+      return true;
+    } else {
+      return false;
+    }
+
+    10 == 10;
+    10 != 9;
 	`
 
 	tests := []struct {
@@ -189,6 +198,31 @@ func TestNextToken3(t *testing.T) {
     {token.GT, ">"},
     {token.INT, "5"},
     {token.SEMICOLON, ";"},
+    {token.IF, "if"},
+    {token.LPAREN, "("},
+    {token.INT, "5"},
+    {token.LT, "<"},
+    {token.INT, "10"},
+    {token.RPAREN, ")"},
+    {token.LBRACE, "{"},
+    {token.RETURN, "return"},
+    {token.TRUE, "true"},
+    {token.SEMICOLON, ";"},
+    {token.RBRACE, "}"},
+    {token.ELSE, "else"},
+    {token.LBRACE, "{"},
+    {token.RETURN, "return"},
+    {token.FALSE, "false"},
+    {token.SEMICOLON, ";"},
+    {token.RBRACE, "}"},
+    {token.INT, "10"},
+    {token.EQ, "=="},
+    {token.INT, "10"},
+		{token.SEMICOLON, ";"},
+    {token.INT, "10"},
+    {token.NOT_EQ, "!="},
+    {token.INT, "9"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 
