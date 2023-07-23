ee346c520a1e91c5059311fba1e31dccb530c321
Стив <kenethra.74@gmail.com>
2023-07-23 17:24:36 -0600

    feat: errors manager

--- parser/parser.go
+++ parser/parser.go
@@ -1,24 +1,38 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/karchx/jj/ast"
 	"github.com/karchx/jj/lexer"
 	"github.com/karchx/jj/token"
 )
 
 type Parser struct {
-	l *lexer.Lexer
-
+	l         *lexer.Lexer
+	errors    []string
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
