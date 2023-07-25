4092ea285f51b910cd9add9afdaff5e0f59808bb
Стив <kenethra.74@gmail.com>
2023-07-24 19:10:28 -0600

    feat: ast return statement

--- ast/ast.go
+++ ast/ast.go
@@ -20,17 +20,6 @@ type Program struct {
 	Statements []Statement
 }
 
-type Identifier struct {
-	Token token.Token
-	Value string
-}
-
-type LetStatement struct {
-	Token token.Token
-	Name  *Identifier
-	Value Expression
-}
-
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -39,10 +28,31 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
 func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (ls *ReturnStatement) statementNode() {}
+func (ls *ReturnStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
